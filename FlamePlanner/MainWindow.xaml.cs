59a0c8a51cc7fb431df7d9f0b9b271003a896872
Nick Zhao <nick.zhao@ucalgary.ca>
2021-10-18 18:37:24 -0600

    change to net core 5.0

--- FlamePlanner/MainWindow.xaml.cs
+++ FlamePlanner/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _481FinalProject_FlamePlanner_
+namespace FlamePlanner
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
