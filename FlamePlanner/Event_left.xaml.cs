5833cfb411759d013a4d06b3794862606973041d
Austin Shawaga <60626400+Retr0-XX@users.noreply.github.com>
2021-10-29 13:01:56 -0600

    Fixed Issues with Recent Merge
    Some things were changed in the main file which should not of been. Reverted main file back to last version and fixed compile issues.
    
    Itinerary content still needs to be connected.

--- FlamePlanner/Event_left.xaml.cs
+++ FlamePlanner/Event_left.xaml.cs
@@ -24,5 +24,10 @@ namespace FlamePlanner
         {
             InitializeComponent();
         }
+
+        private void createNewItineraryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
