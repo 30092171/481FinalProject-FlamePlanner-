b7a85201914e3210d74e7eb4a77acf6eb8f5bb83
Nick Zhao <46830311+30092171@users.noreply.github.com>
2021-10-29 09:14:53 -0600

    Map Pages (#8)
    * empty file - new branch test
    
    * made a map view
    - has dummy zoom buttons
    - scrollable
    - some detailed map of calgary
    - three test POIs
    
    * Added map bottom
    - "filter"
    - "address"
    - "map search"
    - "populated key"
    
    Added a blue marker
    Changed dummy map markers
    
    * Added map events
    
    Merged with Nick's Work
    
    Co-authored-by: Austin Shawaga <60626400+Retr0-XX@users.noreply.github.com>

---
+++ FlamePlanner/Map.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlamePlanner
+{
+    /// <summary>
+    /// Interaction logic for Map.xaml
+    /// </summary>
+    public partial class Map : Page
+    {
+        public Map()
+        {
+            InitializeComponent();
+        }
+        private void poi1_Click(object sender, RoutedEventArgs e)
+        {
+            MapEvent me = new MapEvent("6 Street SW");
+            me.ShowDialog();
+        }
+        private void poi2_Click(object sender, RoutedEventArgs e)
+        {
+            MapEvent me = new MapEvent("Stampede");
+            me.ShowDialog();
+        }
+        private void poi3_Click(object sender, RoutedEventArgs e)
+        {
+            MapEvent me = new MapEvent("Prince Island Park");
+            me.ShowDialog();
+        }
+    }
+}
---
+++ FlamePlanner/MapEvent.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlamePlanner
+{
+    /// <summary>
+    /// Interaction logic for MapEvent.xaml
+    /// </summary>
+    public partial class MapEvent : Window
+    {
+        public MapEvent()
+        {
+            InitializeComponent();
+        }
+        public MapEvent(string eventName)
+        {
+            InitializeComponent();
+            event_label.Content = eventName;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
