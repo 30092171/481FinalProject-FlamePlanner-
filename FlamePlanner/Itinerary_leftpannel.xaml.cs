a927202d98010fbba0948c6b50adb00f7c0fe8b6
Abhay189 <sabhay2001@gmail.com>
2021-10-29 11:46:48 -0600

    Itinerary left panel finalized

--- FlamePlanner/Itinerary_leftpannel.xaml.cs
+++ FlamePlanner/Itinerary_leftpannel.xaml.cs
@@ -24,5 +24,10 @@ namespace FlamePlanner
         {
             InitializeComponent();
         }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
