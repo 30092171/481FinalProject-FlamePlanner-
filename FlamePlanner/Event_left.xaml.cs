46c307eec07b7a8622743a5872a00217db777ce0
Abhay189 <sabhay2001@gmail.com>
2021-10-28 19:45:03 -0600

    Adding Event Screen Left pannel

--- FlamePlanner/Event_left.xaml.cs
+++ FlamePlanner/Event_left.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FlamePlanner
 {
     /// <summary>
-    /// Interaction logic for Itinerary_window.xaml
+    /// Interaction logic for Event_left.xaml
     /// </summary>
-    public partial class Itinerary_window : Window
+    public partial class Event_left : Page
     {
-        public Itinerary_window()
+        public Event_left()
         {
             InitializeComponent();
         }
