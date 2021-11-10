abc6f7ca138aff0400d5630a4bb997d03a949bba
Austin Shawaga <60626400+Retr0-XX@users.noreply.github.com>
2021-11-10 01:31:03 -0700

    Addressed Help Overlay Information Overload
    As Nick suggested, now the information in the help overlay only is revealed when someone mouses over it.
    
    Also fixed the map help overlay referring to scroll bars which are no longer present.

--- FlamePlanner/eventHelpOverlay.xaml.cs
+++ FlamePlanner/eventHelpOverlay.xaml.cs
@@ -32,5 +32,65 @@ namespace FlamePlanner
             mw.helpOverlayFrame.Content = null;
             mw.helpOverlayFrame.Visibility = Visibility.Hidden;
         }
+
+        private void logInHelpTextBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            logInHelpText.Visibility = Visibility.Visible;
+            logInHelpTextBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void logInHelpTextBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            logInHelpText.Visibility = Visibility.Hidden;
+            logInHelpTextBorder.BorderBrush = Brushes.LightCoral;
+        }
+
+        private void currentPageDescriptionBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            currentPageDescription.Visibility = Visibility.Visible;
+            currentPageDescriptionBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void currentPageDescriptionBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            currentPageDescription.Visibility = Visibility.Hidden;
+            currentPageDescriptionBorder.BorderBrush = Brushes.LightCoral;
+        }
+
+        private void navBarDescriptionBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            navBarDescription.Visibility = Visibility.Visible;
+            navBarDescriptionBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void navBarDescriptionBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            navBarDescription.Visibility = Visibility.Hidden;
+            navBarDescriptionBorder.BorderBrush = Brushes.LightCoral;
+        }
+
+        private void leftDescriptionBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            leftDescription.Visibility = Visibility.Visible;
+            leftDescriptionBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void leftDescriptionBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            leftDescription.Visibility = Visibility.Hidden;
+            leftDescriptionBorder.BorderBrush = Brushes.LightCoral;
+        }
+
+        private void bottomDescriptionBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            bottomDescription.Visibility = Visibility.Visible;
+            bottomDescriptionBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void bottomDescriptionBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            bottomDescription.Visibility = Visibility.Hidden;
+            bottomDescriptionBorder.BorderBrush = Brushes.LightCoral;
+        }
     }
 }
--- FlamePlanner/mapHelpOverlay.xaml.cs
+++ FlamePlanner/mapHelpOverlay.xaml.cs
@@ -32,5 +32,65 @@ namespace FlamePlanner
             mw.helpOverlayFrame.Content = null;
             mw.helpOverlayFrame.Visibility = Visibility.Hidden;
         }
+
+        private void navDescriptionBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            navDescription.Visibility = Visibility.Visible;
+            navDescriptionBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void navDescriptionBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            navDescription.Visibility = Visibility.Hidden;
+            navDescriptionBorder.BorderBrush = Brushes.LightCoral;
+        }
+
+        private void logInDescriptionBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            logInDescription.Visibility = Visibility.Visible;
+            logInDescriptionBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void logInDescriptionBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            logInDescription.Visibility = Visibility.Hidden;
+            logInDescriptionBorder.BorderBrush = Brushes.LightCoral;
+        }
+
+        private void screenDescriptionBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            screenDesciption.Visibility = Visibility.Visible;
+            screenDescriptionBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void screenDescriptionBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            screenDesciption.Visibility = Visibility.Hidden;
+            screenDescriptionBorder.BorderBrush = Brushes.LightCoral;
+        }
+
+        private void bottomDescriptionBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            bottomDescription.Visibility = Visibility.Visible;
+            bottomDescriptionBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void bottomDescriptionBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            bottomDescription.Visibility = Visibility.Hidden;
+            bottomDescriptionBorder.BorderBrush = Brushes.LightCoral;
+        }
+
+        private void leftDescriptionBorder_MouseEnter(object sender, MouseEventArgs e)
+        {
+            leftDescription.Visibility = Visibility.Visible;
+            leftDescriptionBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void leftDescriptionBorder_MouseLeave(object sender, MouseEventArgs e)
+        {
+            leftDescription.Visibility = Visibility.Hidden;
+            leftDescriptionBorder.BorderBrush = Brushes.LightCoral;
+        }
     }
 }
