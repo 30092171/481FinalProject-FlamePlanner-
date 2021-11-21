d0f2e603188dffda57aba68e3b799a99c32a3c72
Abhay189 <sabhay2001@gmail.com>
2021-11-21 14:03:20 -0700

    Adding the date functionality

--- FlamePlanner/Itinerarypage.xaml.cs
+++ FlamePlanner/Itinerarypage.xaml.cs
@@ -23,10 +23,86 @@ namespace FlamePlanner
     public partial class Itinerarypage : Page
     {
         private MainWindow mw;
+        private string[] Time_Array = new string[24];   //This array contain all the hours of the day in the correct format. 
+        
+
         public Itinerarypage(MainWindow mw)
         {
             InitializeComponent();
             this.mw = mw;
+            Time_Array_populator();
+            InitialTimePopulator();
+        }
+
+        private void InitialTimePopulator()
+        {
+            string[] Display_time_array = new string[6];
+            for (int i = 1; i <= 6; i++)
+            {
+                Display_time_array[i-1] = i + ":00" + "pm";
+            }
+
+            timerow1.Text = Display_time_array[0];
+            timerow2.Text = Display_time_array[1];
+            timerow3.Text = Display_time_array[2];
+            timerow4.Text = Display_time_array[3];
+            timerow5.Text = Display_time_array[4];
+            timerow6.Text = Display_time_array[5];
+
+        }
+
+        private void Down_Click_time_Updator()
+        {
+            string firstRowTime = timerow1.Text;
+            int i_num = Array.IndexOf(Time_Array,firstRowTime);
+
+            timerow1.Text = Time_Array[(i_num + 1) % 24];
+            timerow2.Text = Time_Array[(i_num + 2) % 24];
+            timerow3.Text = Time_Array[(i_num + 3) % 24];
+            timerow4.Text = Time_Array[(i_num + 4) % 24];
+            timerow5.Text = Time_Array[(i_num + 5) % 24];
+            timerow6.Text = Time_Array[(i_num + 6) % 24];
+        }
+
+        private void Up_Click_time_Updator()
+        {
+            string firstRowTime = timerow1.Text;
+            int i_num = Array.IndexOf(Time_Array, firstRowTime);
+            int change_num;
+            if(i_num == 0)
+            {
+                change_num = Time_Array.Length;
+            }
+            else
+            {
+                change_num = i_num;
+            }
+
+            timerow1.Text = Time_Array[(change_num - 1) % 24];
+            timerow2.Text = Time_Array[(change_num - 2) % 24];
+            timerow3.Text = Time_Array[(change_num - 3) % 24];
+            timerow4.Text = Time_Array[(change_num - 4) % 24];
+            timerow5.Text = Time_Array[(change_num - 5) % 24];
+            timerow6.Text = Time_Array[(change_num - 6) % 24];
+        }
+
+        /*
+         * Populating the time array with the times of the day. 
+         */
+        private void Time_Array_populator()
+        {
+            
+            Time_Array[0] = "12:00am";
+            for (int i = 1; i < 12; i++)
+            {
+                Time_Array[i] = i + ":00" + "am";
+            }
+            Time_Array[12]= 12 + ":00" + "pm";
+
+            for (int i = 1; i < 12; i++)
+            {
+                Time_Array[i + 12] = i + ":00" + "pm";
+            }
         }
 
         private void customEventButton_Click(object sender, RoutedEventArgs e)
@@ -43,11 +119,12 @@ namespace FlamePlanner
         private void Image_MouseLeftButtonDown_TopArrow(object sender, MouseButtonEventArgs e)
         {
             Trace.WriteLine("Clicked Top arrow button");
+            Up_Click_time_Updator();
         }
 
         private void Image_MouseLeftButtonDown_DownArrow(object sender, MouseButtonEventArgs e)
         {
-            Trace.WriteLine("Clicked Down arrow button");
+            Down_Click_time_Updator();
         }
 
         private void Image_MouseLeftButtonDown_LeftArrow(object sender, MouseButtonEventArgs e)
@@ -56,8 +133,13 @@ namespace FlamePlanner
         }
 
         private void Image_MouseLeftButtonDown_RightArrow(object sender, MouseButtonEventArgs e)
-        {
+        {   
             Trace.WriteLine("Clicked Right arrow button");
+
+            for (int i = 0; i < Time_Array.Length; i++)
+            {
+                Trace.WriteLine(Time_Array[i]);
+            }
         }
     }
 }
