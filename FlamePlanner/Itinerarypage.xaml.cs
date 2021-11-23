7ae500c9d52233822d9d813c1951ef746aeb6315
Abhay189 <sabhay2001@gmail.com>
2021-11-22 21:57:28 -0700

    Itinerary Screen Right and left arrow implementation

--- FlamePlanner/Itinerarypage.xaml.cs
+++ FlamePlanner/Itinerarypage.xaml.cs
@@ -23,7 +23,11 @@ namespace FlamePlanner
     public partial class Itinerarypage : Page
     {
         private MainWindow mw;
+        private static int no_of_days = 28;
         private string[] Time_Array = new string[24];   //This array contain all the hours of the day in the correct format. 
+        private DateTime[] Date_Array = new DateTime[no_of_days];
+        private string[] Date_string_Array = new string[no_of_days];
+        
         
         public Itinerarypage(MainWindow mw)
         {
@@ -31,6 +35,9 @@ namespace FlamePlanner
             this.mw = mw;
             Time_Array_populator();
             InitialTimePopulator();
+
+            DateTime startDate = new DateTime(2021,9,5);
+            Date_Array_populator(startDate);
         }
 
         private void InitialTimePopulator()
@@ -67,26 +74,86 @@ namespace FlamePlanner
         {
             string firstRowTime = timerow1.Text;
             int i_num = Array.IndexOf(Time_Array, firstRowTime);
-            int change_num = i_num;
 
-            if(change_num > 0)
+            if(i_num > 0)
             {
-                timerow1.Text = Time_Array[(change_num - 1)];
-                timerow2.Text = Time_Array[(change_num)];
-                timerow3.Text = Time_Array[(change_num + 1) % 24];
-                timerow4.Text = Time_Array[(change_num + 2) % 24];
-                timerow5.Text = Time_Array[(change_num + 3) % 24];
-                timerow6.Text = Time_Array[(change_num + 4) % 24];
+                timerow1.Text = Time_Array[(i_num - 1)];
+                timerow2.Text = Time_Array[(i_num)];
+                timerow3.Text = Time_Array[(i_num + 1) % 24];
+                timerow4.Text = Time_Array[(i_num + 2) % 24];
+                timerow5.Text = Time_Array[(i_num + 3) % 24];
+                timerow6.Text = Time_Array[(i_num + 4) % 24];
             }
             
-            if (change_num == 0)
+            if (i_num == 0)
             {
                 timerow1.Text = Time_Array[Time_Array.Length - 1];
-                timerow2.Text = Time_Array[(change_num)];
-                timerow3.Text = Time_Array[(change_num + 1) % 24];
-                timerow4.Text = Time_Array[(change_num + 2) % 24];
-                timerow5.Text = Time_Array[(change_num + 3) % 24];
-                timerow6.Text = Time_Array[(change_num + 4) % 24];
+                timerow2.Text = Time_Array[(i_num)];
+                timerow3.Text = Time_Array[(i_num + 1) % 24];
+                timerow4.Text = Time_Array[(i_num + 2) % 24];
+                timerow5.Text = Time_Array[(i_num + 3) % 24];
+                timerow6.Text = Time_Array[(i_num + 4) % 24];
+            }
+
+        }
+
+        private void Right_Click_date_Updator()
+        {
+            string firstColTime = DateCol1.Text;
+            Trace.WriteLine(DateCol1.Text);
+            Trace.WriteLine(Date_string_Array[1]);
+            int i_num = Array.IndexOf(Date_string_Array, firstColTime);
+
+            DateCol1.Text = Date_Array[(i_num + 1) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 1) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 1) % no_of_days].ToString("MMM");
+            DateCol2.Text = Date_Array[(i_num + 2) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 2) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 2) % no_of_days].ToString("MMM");
+            DateCol3.Text = Date_Array[(i_num + 3) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 3) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 3) % no_of_days].ToString("MMM");
+            DateCol4.Text = Date_Array[(i_num + 4) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 4) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 4) % no_of_days].ToString("MMM");
+            DateCol5.Text = Date_Array[(i_num + 5) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 5) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 5) % no_of_days].ToString("MMM");
+            DateCol6.Text = Date_Array[(i_num + 6) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 6) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 6) % no_of_days].ToString("MMM");
+            DateCol7.Text = Date_Array[(i_num + 7) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 7) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 7) % no_of_days].ToString("MMM");
+        }
+
+        private void Left_Click_date_Updatoe()
+        {
+            string firstColTime = DateCol1.Text;
+            int i_num = Array.IndexOf(Date_string_Array, firstColTime);
+
+            if (i_num > 0)
+            {
+                DateCol1.Text = Date_Array[(i_num - 1) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num - 1) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num - 1) % no_of_days].ToString("MMM");
+                DateCol2.Text = Date_Array[i_num % no_of_days].ToString("ddd") + " " + Date_Array[i_num % no_of_days].Day.ToString() + ", " + Date_Array[i_num % no_of_days].ToString("MMM");
+                DateCol3.Text = Date_Array[(i_num + 1) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 1) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 1) % no_of_days].ToString("MMM");
+                DateCol4.Text = Date_Array[(i_num + 2) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 2) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 2) % no_of_days].ToString("MMM");
+                DateCol5.Text = Date_Array[(i_num + 3) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 3) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 3) % no_of_days].ToString("MMM");
+                DateCol6.Text = Date_Array[(i_num + 4) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 4) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 4) % no_of_days].ToString("MMM");
+                DateCol7.Text = Date_Array[(i_num + 5) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 5) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 5) % no_of_days].ToString("MMM");
+                
+
+            }
+
+            if (i_num == 0)
+            {
+                DateCol1.Text = Date_Array[no_of_days - 1].ToString("ddd") + " " + Date_Array[no_of_days - 1].Day.ToString() + ", " + Date_Array[no_of_days - 1].ToString("MMM");
+                DateCol2.Text = Date_Array[i_num % no_of_days].ToString("ddd") + " " + Date_Array[i_num % no_of_days].Day.ToString() + ", " + Date_Array[i_num % no_of_days].ToString("MMM");
+                DateCol3.Text = Date_Array[(i_num + 1) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 1) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 1) % no_of_days].ToString("MMM");
+                DateCol4.Text = Date_Array[(i_num + 2) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 2) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 2) % no_of_days].ToString("MMM");
+                DateCol5.Text = Date_Array[(i_num + 3) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 3) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 3) % no_of_days].ToString("MMM");
+                DateCol6.Text = Date_Array[(i_num + 4) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 4) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 4) % no_of_days].ToString("MMM");
+                DateCol7.Text = Date_Array[(i_num + 5) % no_of_days].ToString("ddd") + " " + Date_Array[(i_num + 5) % no_of_days].Day.ToString() + ", " + Date_Array[(i_num + 5) % no_of_days].ToString("MMM");
+                
+
+            }
+        }
+
+        private void Date_Array_populator(DateTime start_date)
+        {
+            int i = 0;
+            DateTime date = start_date;
+            while( i < Date_Array.Length)
+            {
+                Date_Array[i] = date.AddDays(i);
+                Date_string_Array[i] = Date_Array[i].ToString("ddd") + " " + Date_Array[i].Day.ToString() + ", " + Date_Array[i].ToString("MMM");
+                i++;
             }
 
         }
@@ -116,14 +183,9 @@ namespace FlamePlanner
             ceiw.ShowDialog();
         }
 
-        private void TimeUpButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void Image_MouseLeftButtonDown_TopArrow(object sender, MouseButtonEventArgs e)
         {
-            Trace.WriteLine("Clicked Top arrow button");
             Up_Click_time_Updator();
         }
 
@@ -134,17 +196,12 @@ namespace FlamePlanner
 
         private void Image_MouseLeftButtonDown_LeftArrow(object sender, MouseButtonEventArgs e)
         {
-            Trace.WriteLine("Clicked Left arrow button");
+            Left_Click_date_Updatoe();
         }
 
         private void Image_MouseLeftButtonDown_RightArrow(object sender, MouseButtonEventArgs e)
-        {   
-            Trace.WriteLine("Clicked Right arrow button");
-
-            for (int i = 0; i < Time_Array.Length; i++)
-            {
-                Trace.WriteLine(Time_Array[i]);
-            }
+        {
+            Right_Click_date_Updator();
         }
     }
 }
