72a32d1e4e674973f6f9b23de01a6fe7fa0c0dd2
Austin Shawaga <60626400+Retr0-XX@users.noreply.github.com>
2021-11-24 15:03:19 -0700

    Removed Restrictions from EventObject

--- FlamePlanner/EventObject.cs
+++ FlamePlanner/EventObject.cs
@@ -20,9 +20,6 @@ namespace FlamePlanner
         public int startTime = 0;  //24 hr 1300 = 1:00pm and so on
         public int endTime = 0; //24 hr 900 = 9:00am and so on
 
-        public bool isRestricted = false;
-        public int[,] restrictions = new int[7,2]; //7 x 2 array where each row represents a day starting at sunday and col 0 represents start restriction and col 1 represents end restriction
-
         public bool isVisible = true; //boolean to represent whether or not event should be displayed
 
         public int filterID = 0; //0 = no filter, integer used to identify filter criteria in iterinary
@@ -53,28 +50,6 @@ namespace FlamePlanner
             this.startDate = date;
             this.startTime = sTime;
             this.endTime = eTime;
-            this.isRestricted = false;
-        }
-
-        /// <summary>
-        /// Constructor for Event Class which populates all fields for general date/time with restrictions
-        /// </summary>
-        /// <param name="name">Represents Event Name as a string</param>
-        /// <param name="details">Represents Event Details as a string</param>
-        /// <param name="location">Represents location of event as a string</param>
-        /// <param name="r">[7,2] integer array representing restrictions for each day of week [0,] = Sunday</param>
-        public EventObject(string name, string details, string location, int[,] r)
-        {
-            this.eventName = name;
-            this.eventDetails = details;
-            this.eventLocation = location;
-
-            if(r.Length != 14)
-            {
-                throw new Exception("Invalid Restriction Argument");
-            }
-            this.restrictions = r;
-            this.isRestricted = true;
         }
 
 
